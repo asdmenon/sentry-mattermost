ad72ba6285f1a855faaf127aea71d8284809aa17
asdmenon <aditya.menon@kuwaitnet.dev>
2023-07-02 17:46:24 +0530

    fix: remove "," and format

--- sentry_mattermost/__init__.py
+++ sentry_mattermost/__init__.py
@@ -1,5 +1,4 @@
 try:
-    VERSION = __import__('pkg_resources') \
-        .get_distribution(__name__).version
-except Exception, e:
-    VERSION = '0.0.1'
+    VERSION = __import__("pkg_resources").get_distribution(__name__).version
+except Exception as e:
+    VERSION = "0.0.1"
